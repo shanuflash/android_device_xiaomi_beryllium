22c8cd0c96673b5f508be2900bdc00beaaa792d9
ganeshi4u <ganeshvarmai4u@gmail.com>
2018-10-22 15:55:23 +0200

    beryllium: add some props

--- device-props.mk
+++ device-props.mk
@@ -4,7 +4,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.multicam.fpsmatch=TRUE \
     persist.vendor.camera.multicam.hwsync=TRUE \
     persist.vendor.camera.multicam.framesync=1 \
-    persist.vendor.camera.perfcapture=1
+    persist.vendor.camera.perfcapture=1 \
+    camera.disable_zsl_mode=true
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -14,3 +15,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qcom.ad.sensortype=2 \
     ro.qualcomm.cabl=0 \
     ro.sf.lcd_density=480
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.use_buffer_age=false \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1
